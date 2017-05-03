b6d3de80f39c024753dfd9dccba4cd6854074a4f
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-03 15:59:37 -0700

    adding list filters to Class-based View functions to filter out future questions

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # some django shortcuts ================================
 from django.shortcuts import render, get_object_or_404
@@ -62,18 +63,30 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list' # need to override default context name
     def get_queryset(self):
-      return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 # ===== generic.DetailView automatically provides 'question' context variable
 class DetailView(generic.DetailView):
     model = Question # generic view needs to know this
     template_name = 'polls/detail.html' # provides specific name to template
+    def get_queryset(self):
+        """ excludes questions that aren't published yet """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        )
 
 
 class ResultsView(generic.DetailView):
     model = Question # generic view needs to know this
     template_name = 'polls/results.html' # provides specific name to template
+    def get_queryset(self):
+        """ excludes questions that aren't published yet """
+        return Question.objects.filter(
+          pub_date__lte=timezone.now()
+        )
 
 
 #  VOTE controller - post request #
