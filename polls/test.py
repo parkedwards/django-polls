34c257a2785baadd2e85b61bcef621b1038fb586
EDWARD PARK <ed.sh.park@gmail.com>
2017-05-03 14:22:48 -0700

    adding unit tests for was_published_recently

--- polls/test.py
+++ polls/test.py
@@ -4,3 +4,22 @@ from django.test import TestCase
 
 from .models import Question
 
+
+class DateTimeTestCase(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """ should return false for questions whose pub_date is in the future """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """ should return false for questions whose pub_date is older than 1 day """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """ should return True for questions whose pub_date is within the last day """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
