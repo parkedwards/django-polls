1f89d5c0c86416affbdf8cb71ac39eb92d59c03c
EDWARD PARK <edwardpark@EDWARDs-MBP.lan>
2017-05-03 12:04:28 -0700

    adding comments to routing module

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
+# Note - 'views' acts as the controller functions
+# They they will call the render method + the templates that need to be rendered
 from . import views
 
 app_name = 'polls' # this is a URL namespace
